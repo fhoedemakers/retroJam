c076eef1e530a8a8f746944f12346bf2c7fdd9cf
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2025-12-30 14:55:09 +0100

    added build scripts

---
+++ buildAll.sh
@@ -0,0 +1,31 @@
+:
+# ====================================================================================
+# retroJam build all script 
+# Only Adafruit Fruit Jam supported.
+# Binaries are copied to the releases folder
+# ====================================================================================
+cd `dirname $0` || exit 1
+[ -d releases ] && rm -rf releases
+mkdir releases || exit 1
+# check picotool exists in path
+if ! command -v picotool &> /dev/null
+then
+	echo "picotool could not be found"
+	echo "Please install picotool from https://github.com/raspberrypi/picotool.git" 
+	exit
+fi
+HWCONFIGS="8"
+for HWCONFIG in $HWCONFIGS
+do
+	./bld.sh -c $HWCONFIG -2 || exit 1
+done
+if [ -z "$(ls -A releases)" ]; then
+	echo "No UF2 files found in releases folder"
+	exit 1
+fi
+for UF2 in releases/*.uf2
+do
+	ls -l $UF2
+	picotool info $UF2
+	echo " "
+done
